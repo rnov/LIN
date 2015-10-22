cee9378629cba8a0fa1f9793218a78ccbddbefa7
kernel <kernel@debian>
2015-10-22 20:29:13 +0200

    Practica terminada

--- modlist/modlist.c
+++ modlist/modlist.c
@@ -9,8 +9,8 @@ MODULE_LICENSE("GPL"); /* Licencia del modulo */
 
 /* Nodos de la lista */
 typedef struct {
-	int data;
-	struct list_head links;
+    int data;
+    struct list_head links;
 }list_item_t;
 
 struct list_head mylist; /* Lista enlazada */
@@ -19,51 +19,84 @@ static struct proc_dir_entry *proc_entry;
 
 #define MAX_CHARS = 10;
 
-#define Foreach(it, tmp, list) \
-     { \
-        struct list_head *it, *tmp; \
-  		list_item_t *elem; \
-  		list_for_each_safe(it, tmp, list) {
+#define Foreach(it, tmp, list)            \
+     {                                    \
+        struct list_head *it, *tmp;       \
+        list_item_t *elem;                \
+    list_for_each_safe(it, tmp, list) {
 
 #define Endforeach() }}
 
 /* To do*/
 static ssize_t list_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+    char kbuf[len]; int num;
 
-	char kbuf[len]; int num;
+    /* Transfer data from user to kernel space */
+        if (copy_from_user(&kbuf, buf, len))  
+        return -EFAULT;
 
-	/* Transfer data from user to kernel space */
-  	if (copy_from_user(&kbuf, buf, len))  
-    	return -EFAULT;
+    if(sscanf(kbuf, "add %i", &num) == 1) {  // add command
+        trace_printk("Executing command 'add %i'\n", num);
+        
+        list_item_t *new_node;
+        new_node = vmalloc(sizeof(list_item_t));
+        new_node->data = num;
+        
+        list_add_tail(&(new_node->links), &mylist); 
+    }else if(sscanf(kbuf, "remove %i", &num) == 1){  // remove command, 
+        trace_printk("Executing command 'remove %i'\n", num);
+        Foreach(pos, aux, &mylist)
+            elem = list_entry(pos, list_item_t, links);
+            trace_printk("At elem '%i' (@%p)\n", elem->data, elem);
 
-	if(sscanf(kbuf, "add %i", &num) == 1) {  // add command
-		trace_printk("Executing command 'add %i'", num);
-		
-		list_item_t *new_node;
-    	new_node = vmalloc(sizeof(list_item_t));
-    	new_node->data = num;
-		list_add_tail(&(new_node->links), &mylist); 
-	}else if(sscanf(kbuf, "remove %i", &num) == 1){  // remove command, 
-		trace_printk("Executing command 'remove %i'", num);
-	  	Foreach(pos, aux, &mylist)
-			elem = list_entry(pos, list_item_t, links);
-			if(elem->data == num){  
-				list_del(pos);
-				vfree(elem);
-			}
-	  	Endforeach()
-	}else
-		printk(KERN_INFO "Unknown command");
+            if(elem->data == num){
+                trace_printk("Found elem '%i'. Removing...\n", elem->data);
+                list_del(pos);
+                vfree(elem);
+            }
+        Endforeach()
+    }else
+        printk(KERN_INFO "Unknown command");
 
-	(*off)+=len;
+    (*off)+=len;
 
-	return len;
+    return len;
 }
 
 /* To do*/
 static ssize_t list_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+    char kbuff[1024] = {};
+    char* buffpos = &kbuff[0];
 
-    return 0;
+    if((*off) > 0)
+        return 0;
+
+    buffpos += sprintf(buffpos, "(");
+
+    Foreach(pos, aux, &mylist)
+        elem = list_entry(pos, list_item_t, links);
+        trace_printk("At elem '%i' (@%p)\n", elem->data, elem);
+        
+        buffpos += sprintf(buffpos, "%i,", elem->data);
+    Endforeach()
+
+    if(buffpos > &kbuff[0] + 1)
+        buffpos--; // Go back one byte to overwrite last colon
+
+    buffpos += sprintf(buffpos, ")\n");
+
+    size_t written = min(len, buffpos - &kbuff[0]);
+
+    kbuff[written] = '\0';
+
+    trace_printk("Output: '%s'", kbuff);
+
+    if(copy_to_user(buf, kbuff, written))
+        return -EINVAL;
+    
+    *off += written;
+
+    return written;
 }
 
 /* Especifica operaciones /proc se implementan */
@@ -75,18 +108,18 @@ static const struct file_operations proc_entry_fops = {
 /* done?*/
 int init_list_module( void )
 {
-	int ret = 0;
+    int ret = 0;
     proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
     if(proc_entry == NULL){
-    	ret = -ENOMEM;
-    	printk(KERN_INFO "modlist: Can't create /proc entry\n");
+        ret = -ENOMEM;
+        printk(KERN_INFO "modlist: Can't create /proc entry\n");
     }else{
-	    /* Inicializamos la lista*/
-		INIT_LIST_HEAD(&mylist);
-		printk(KERN_INFO "modlist: Module loaded\n");
-	}
-	
-	return ret;
+        /* Inicializamos la lista*/
+        INIT_LIST_HEAD(&mylist);
+        printk(KERN_INFO "modlist: Module loaded\n");
+    }
+    
+    return ret;
 }
 
 /* done*/
@@ -96,9 +129,9 @@ void exit_list_module( void )
   struct list_head *pos, *aux;
   list_item_t *tmp;
   list_for_each_safe(pos, aux, &mylist){
-	  tmp = list_entry(pos, list_item_t, links);
-	  list_del(pos);
-	  vfree(tmp);
+      tmp = list_entry(pos, list_item_t, links);
+      list_del(pos);
+      vfree(tmp);
   }
   printk(KERN_INFO "modlist: Module unloaded.\n");
 }
@@ -111,16 +144,16 @@ module_exit(exit_list_module);
 
 /**
  * list_entry - get the struct for this entry
- * @ptr:	the &struct list_head pointer.
- * @type:	the type of the struct this is embedded in.
- * @member:	the name of the list_struct within the struct.
+ * @ptr:    the &struct list_head pointer.
+ * @type:   the type of the struct this is embedded in.
+ * @member: the name of the list_struct within the struct.
  */
 
  /**
- * list_for_each_safe	-	iterate over a list safe against removal of list entry
- * @pos:	the &struct list_head to use as a loop counter.
- * @n:		another &struct list_head to use as temporary storage
- * @head:	the head for your list.
+ * list_for_each_safe   -   iterate over a list safe against removal of list entry
+ * @pos:    the &struct list_head to use as a loop counter.
+ * @n:      another &struct list_head to use as temporary storage
+ * @head:   the head for your list.
  */
 
 /**
