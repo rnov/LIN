efdc8518686a70218b95e79193b4b290032d1675
rnov <rsmarinov@gmail.com>
2015-11-19 17:29:01 +0100

    avance apratado B

--- FicherosP2/Modleds/modleds.c
+++ FicherosP2/Modleds/modleds.c
@@ -4,13 +4,13 @@
 #include <linux/tty.h>      /* For fg_console */
 #include <linux/kd.h>       /* For KDSETLED */
 #include <linux/vt_kern.h>
+#include <linux/proc_fs.h>
 
 #define ALL_LEDS_ON 0x7
 #define ALL_LEDS_OFF 0
 
-
-struct tty_driver* kbd_driver= NULL;
-
+static struct proc_dir_entry *proc_entry;
+static struct tty_driver* kbd_driver= NULL;
 
 /* Get driver handler */
 struct tty_driver* get_kbd_driver_handler(void){
@@ -24,14 +24,42 @@ static inline int set_leds(struct tty_driver* handler, unsigned int mask){
     return (handler->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,mask);
 }
 
+static ssize_t key_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+	
+	unsigned char kbuf [4] = {}; /*No meter valores mayores a 4Bytes*/
+	unsigned int aux;
+	
+	/* Transfer data from user to kernel space */
+	if (copy_from_user(kbuf, buf, len )!=0)  
+		return -ENOSPC;	
+	
+	kbuf[len] = '\0'; /* Add the `\0' */
+	
+	sscanf(kbuf,"%x",&aux);
+	
+	if(aux<0 || aux>7 ){  /* Fuera de rango */
+		set_leds(kbd_driver,ALL_LEDS_OFF);		
+	}else{
+		set_leds(kbd_driver,aux);
+		}
+	
+	*off+=len;        /* Update the file pointer */
+	return len;
+}
+
+static const struct file_operations proc_entry_fops = {
+	.write = key_write,    
+};
+
 static int __init modleds_init(void)
 {	
+   proc_entry = proc_create( "modleds", 0666, NULL, &proc_entry_fops);
    kbd_driver= get_kbd_driver_handler();
-   set_leds(kbd_driver,ALL_LEDS_ON); 
    return 0;
 }
 
 static void __exit modleds_exit(void){
+    remove_proc_entry("modleds", NULL);
     set_leds(kbd_driver,ALL_LEDS_OFF); 
 }
 
