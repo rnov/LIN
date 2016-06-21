42cdcb6ff11cdf0df44304dfd6809d3f5dade68a
rnov <rsmarinov@gmail.com>
2016-06-22 01:17:41 +0200

    practica5 acabada (examen)

--- practica3/parte1/script2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 210 );
-	do 
-		sleep 0.02
-		echo add  $i > /proc/modlist;
-		cat /proc/modlist;
-done
--- practica3/parte1/script3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 200 );
-	do 
-		sleep 0.02
-		echo remove  $i > /proc/modlist;
-done
